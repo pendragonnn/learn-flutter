62004b2727581dd851d6d5dc0ba09b5ea7971b6e
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2024-11-03 09:57:27 +0700

    add topic 2 - Basic Widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp()); // fungsi untuk menjalankan aplikasi dalam bentuk class MyApp
 }
+
 /**
 * widget MaterialApp merupakan widget yang paling penting, karena widget ini berfungsi sebagai
 * wadah kita dalam membuat tampilan aplikasi. kalau dianalogikan, widget ini berfungsi
@@ -19,18 +20,61 @@ void main() {
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      /**
+        /**
       * Jika MaterialApp merupakan papan kayu untuk menulis, maka widget Scaffold ini
       * bisa diumpamakan sebagai canvas untuk melukisnya. untuk membuat lukisan yang baru,
       * kita bisa mengganti canvas ini. sama halnya dengan mengganti halaman (page) 
       * pada Flutter. kita memerlukan widget scaffold lagi pada page yang baru.
       */
-      home: Scaffold(
-        backgroundColor: Colors.amber[200], // widget untuk memberi warna latar belakang
-        body: const Center( // widget untuk memposisikan anak komponennya ke tengah secara horizontal dan vertikal
-          child: Text("Hello World"), // widget untuk menampilkan teks
-        ),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              // widget untuk menambahkan app bar
+              backgroundColor: Colors.red[900],
+              title: const Text(
+                  "Aplikasi Pertamaku"), // menambahkan judul dalam appbar
+              centerTitle: true, // membuat title berada di tengah
+            ),
+            backgroundColor:
+                Colors.white, // widget untuk memberi warna latar belakang
+            body:
+                // Center(
+                //     // widget untuk memposisikan anak komponennya ke tengah secara horizontal dan vertikal
+                //     child: Container(
+                //         // menambahkan container
+                //         width: 200,
+                //         height: 50,
+                //         color: Colors.amber,
+                //         child: const Center(
+                //           child: Text("Hello World"), // widget untuk menampilkan teks
+                //         ))),
+                Center(
+              // child: const FlutterLogo( // menambahkan logo
+              // size: 200,
+              // child: ElevatedButton( // widget untuk tombol
+              //     onPressed: () {
+              //       // menjalankan perintah ketika tombol diklik
+              //       print("Clicked");
+              //     },
+              //     child: Text("Click Saya"))
+              // child: Icon(
+              //   Icons.icecream_outlined,
+              //   size: 200,
+              //   color: Colors.red[200],
+              // ), // widget untuk menambahkan icon
+              // child: Image(
+              //     // image provider:
+              //     // 1. asset image -> gambar yang ada pada folder project --> memerlukan pendaftaran pada pubspec.yaml
+              //     // 2. network image -> mengambil gambar dari internet (wajib online)
+              //     // image: AssetImage("assets/hehe.png")),
+
+              //     // jarang digunakan
+              //     // 3. File Image
+              //     // 4. Memory Image
+              //     image: NetworkImage(
+              //         "https://c4.wallpaperflare.com/wallpaper/110/521/171/artwork-chainsaw-man-makima-chainsaw-man-anime-girls-hd-wallpaper-preview.jpg")),
+
+              // cara cepat
+              child: Image.asset("assets/hehe.png"),
+            )));
   }
 }
