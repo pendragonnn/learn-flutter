014ff45d68f3c2205349ea7016662b25b205c11a
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2024-11-04 08:46:05 +0700

    add topic 3 - Visible vs Invisible Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,423 @@
 import 'package:flutter/material.dart';
-/**
-* Semua class pada Flutter bisa kita sebut sebagai widget. kalau dianalogitkan, jika di meja
-* kita sekarang ada secangkir kopi, maka kopi tersebut adalah widget. namun,perlu diperhatikan
-* dari secangkir kopi tersebut, bahwa itu terdiri dari widget cangkir, widget kopi, widget air,
-* dan widget gula. oleh karena itu, untuk menjadi flutter developer sejati, kita harus mengetahui
-* beragam widget sebanyak-banyaknya.
-*/
 
 void main() {
-  runApp(MyApp()); // fungsi untuk menjalankan aplikasi dalam bentuk class MyApp
+  runApp(MyApp2());
 }
 
-/**
-* widget MaterialApp merupakan widget yang paling penting, karena widget ini berfungsi sebagai
-* wadah kita dalam membuat tampilan aplikasi. kalau dianalogikan, widget ini berfungsi
-* sebagai papan kayu untuk melukis.
-*/
+class MyApp2 extends StatelessWidget {
+  const MyApp2({super.key});
 
-class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        /**
-      * Jika MaterialApp merupakan papan kayu untuk menulis, maka widget Scaffold ini
-      * bisa diumpamakan sebagai canvas untuk melukisnya. untuk membuat lukisan yang baru,
-      * kita bisa mengganti canvas ini. sama halnya dengan mengganti halaman (page) 
-      * pada Flutter. kita memerlukan widget scaffold lagi pada page yang baru.
-      */
-        home: Scaffold(
-            appBar: AppBar(
-              // widget untuk menambahkan app bar
-              backgroundColor: Colors.red[900],
-              title: const Text(
-                  "Aplikasi Pertamaku"), // menambahkan judul dalam appbar
-              centerTitle: true, // membuat title berada di tengah
+      home: Scaffold(
+        // app bar & text termasuk dalam visible widget
+        appBar: AppBar(title: Text("Judul"), backgroundColor: Colors.black12),
+        // body: Center(
+        //   // Center termasuk Invisible widget (tak terlihat)
+        //   child: Text("Hallo"),
+        // ),
+        // body: Container(
+        //   // container bisa terlihat bisa tidak, tidak terlihat jika tidak diberikan ukuran & warna
+        //   width: 100,
+        //   height: 100,
+        //   color: Colors.amber[200],
+        //   child: Center(
+        //     child: Text("halo"),
+        //   ),
+        // ),
+        // untuk menyusun satu atau lebih widget secara vertikal, maka widget ini adalah pilihan yang tepat
+        // body: Column(
+        //   // termasuk invisible widget
+        //   children: [
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.amber[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.red[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.green[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.blue[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //   ],
+        // ),
+        // untuk menyusun satu atau lebih widget secara horizontal, maka widget ini adalah pilihan yang tepat
+        // body: Row(
+        //   // termasuk invisible widget
+        //   children: [
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.amber[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.red[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.green[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.blue[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //   ],
+        // ),
+        // untuk menyusun satu atau lebih widget pada sumbu Z (depan - belakang)
+        // body: Stack(
+        //   // termasuk invisible widget
+        //   children: [
+        //     Container(
+        //       width: 200,
+        //       height: 200,
+        //       color: Colors.amber[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 150,
+        //       height: 150,
+        //       color: Colors.red[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.green[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //   ],
+        // ),
+        //
+        // body: SingleChildScrollView(
+        //   // secara default scroll direction dari widget ini adalah vertikal
+        //   scrollDirection: Axis.horizontal,
+        //   child: Row(
+        //     // termasuk invisible widget
+        //     children: [
+        //       Container(
+        //         width: 100,
+        //         height: 100,
+        //         color: Colors.amber[200],
+        //         child: Center(
+        //           child: Text("halo"),
+        //         ),
+        //       ),
+        //       Container(
+        //         width: 100,
+        //         height: 100,
+        //         color: Colors.red[200],
+        //         child: Center(
+        //           child: Text("halo"),
+        //         ),
+        //       ),
+        //       Container(
+        //         width: 100,
+        //         height: 100,
+        //         color: Colors.green[200],
+        //         child: Center(
+        //           child: Text("halo"),
+        //         ),
+        //       ),
+        //       Container(
+        //         width: 100,
+        //         height: 100,
+        //         color: Colors.blue[200],
+        //         child: Center(
+        //           child: Text("halo"),
+        //         ),
+        //       ),
+        //       Container(
+        //         width: 100,
+        //         height: 100,
+        //         color: Colors.amber[200],
+        //         child: Center(
+        //           child: Text("halo"),
+        //         ),
+        //       ),
+        //       Container(
+        //         width: 100,
+        //         height: 100,
+        //         color: Colors.red[200],
+        //         child: Center(
+        //           child: Text("halo"),
+        //         ),
+        //       ),
+        //       Container(
+        //         width: 100,
+        //         height: 100,
+        //         color: Colors.green[200],
+        //         child: Center(
+        //           child: Text("halo"),
+        //         ),
+        //       ),
+        //       Container(
+        //         width: 100,
+        //         height: 100,
+        //         color: Colors.blue[200],
+        //         child: Center(
+        //           child: Text("halo"),
+        //         ),
+        //       ),
+        //     ],
+        //   ),
+        // )
+        // body: ListView(
+        //   // termasuk invisible widget
+        //   // memiliki scroll direction default bernilai vertical
+        //   // jika horizontal, semua anak yang ada dalam widget ini tingginya mengikuti layar (tidak dapat diubah) begitupun sebaliknya
+        //   // hanya width saja yang bisa diubah
+        //   scrollDirection: Axis.horizontal,
+        //   children: [
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.amber[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.red[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.green[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.blue[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.amber[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.red[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.green[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.blue[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //   ],
+        // )
+
+        // body: Column(
+        //   // mengatur tata letak secara vertikal - jika widget column, namun sebaliknya jika row
+        //   mainAxisAlignment: MainAxisAlignment.end,
+        //   // mengatur tata letak secara horizontal - jika widget column, namun sebaliknya jika row
+        //   crossAxisAlignment: CrossAxisAlignment.end,
+        //   children: [
+        //     Container(
+        //       width: 150,
+        //       height: 150,
+        //       color: Colors.amber[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.red[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 200,
+        //       height: 200,
+        //       color: Colors.green[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //   ],
+        // ),
+        // body: Row(
+        //   // mengatur tata letak secara horizontal - jika widget row, namun sebaliknya jika column
+        //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //   // mengatur tata letak secara vertikal - jika widget row, namun sebaliknya jika column
+        //   crossAxisAlignment: CrossAxisAlignment.center,
+        //   children: [
+        //     Container(
+        //       width: 150,
+        //       height: 150,
+        //       color: Colors.amber[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.red[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 50,
+        //       height: 50,
+        //       color: Colors.green[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //   ],
+        // ),
+        // body: Stack(
+        //   // mengatur tata letak di widget stack
+        //   alignment: AlignmentDirectional.topEnd,
+        //   children: [
+        //     Container(
+        //       width: 150,
+        //       height: 150,
+        //       color: Colors.amber[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 100,
+        //       height: 100,
+        //       color: Colors.red[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //     Container(
+        //       width: 50,
+        //       height: 50,
+        //       color: Colors.green[200],
+        //       child: Center(
+        //         child: Text("halo"),
+        //       ),
+        //     ),
+        //   ],
+        // ),
+
+        // Grid View
+        body: GridView(
+          // memberikan padding
+          // padding: EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 20), // satu satu
+          padding: EdgeInsets.all(10),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, // ukuran grid ditentukan di sini
+            crossAxisSpacing: 5, // spacing secara horizontal
+            mainAxisSpacing: 5, // spacing secar vertikal
+            // childAspectRatio: 1 / 2,
+          ),
+          // SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 200),
+          children: [
+            Container(
+              // tidak berefek
+              // width: 100,
+              // height: 100,
+              color: Colors.amber,
             ),
-            backgroundColor:
-                Colors.white, // widget untuk memberi warna latar belakang
-            body:
-                // Center(
-                //     // widget untuk memposisikan anak komponennya ke tengah secara horizontal dan vertikal
-                //     child: Container(
-                //         // menambahkan container
-                //         width: 200,
-                //         height: 50,
-                //         color: Colors.amber,
-                //         child: const Center(
-                //           child: Text("Hello World"), // widget untuk menampilkan teks
-                //         ))),
-                Center(
-              // child: const FlutterLogo( // menambahkan logo
-              // size: 200,
-              // child: ElevatedButton( // widget untuk tombol
-              //     onPressed: () {
-              //       // menjalankan perintah ketika tombol diklik
-              //       print("Clicked");
-              //     },
-              //     child: Text("Click Saya"))
-              // child: Icon(
-              //   Icons.icecream_outlined,
-              //   size: 200,
-              //   color: Colors.red[200],
-              // ), // widget untuk menambahkan icon
-              // child: Image(
-              //     // image provider:
-              //     // 1. asset image -> gambar yang ada pada folder project --> memerlukan pendaftaran pada pubspec.yaml
-              //     // 2. network image -> mengambil gambar dari internet (wajib online)
-              //     // image: AssetImage("assets/hehe.png")),
-
-              //     // jarang digunakan
-              //     // 3. File Image
-              //     // 4. Memory Image
-              //     image: NetworkImage(
-              //         "https://c4.wallpaperflare.com/wallpaper/110/521/171/artwork-chainsaw-man-makima-chainsaw-man-anime-girls-hd-wallpaper-preview.jpg")),
-
-              // cara cepat
-              child: Image.asset("assets/hehe.png"),
-            )));
+            Container(
+              color: Colors.blue,
+            ),
+            Container(
+              color: Colors.red,
+            ),
+            Container(
+              color: Colors.pink,
+            ),
+            Container(
+              color: Colors.amber,
+            ),
+            Container(
+              color: Colors.blue,
+            ),
+            Container(
+              color: Colors.red,
+            ),
+            Container(
+              color: Colors.pink,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
