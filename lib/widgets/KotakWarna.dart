2ffd15a340a13ade94af90b3acac6bde8125a8f6
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2024-11-04 13:46:02 +0700

    add topic 4 - Extract Widget

---
+++ lib/widgets/KotakWarna.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// reusable widget or extract widget
+class KotakWarna extends StatelessWidget {
+  const KotakWarna({super.key, required this.text, required this.warna});
+
+  final String text;
+  final Color warna;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      color: warna,
+      child: Center(
+        child: Text(text),
+      ),
+    );
+  }
+}
